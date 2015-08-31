6013a0d16397f67f0134e1f40206ce392d12ca06
mikemajzoub <mikemajzoub@gmail.com>
2015-08-30 22:27:56 -0400

    begin encapsulation of OpenEars

--- DoorwayUsa0/DoorwayUsa0-Bridging-Header.h
+++ DoorwayUsa0/DoorwayUsa0-Bridging-Header.h
@@ -1,8 +1,9 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-
-#import <OpenEars/OELanguageModelGenerator.h>
-#import <OpenEars/OEAcousticModel.h>
 #import <OpenEars/OEPocketsphinxController.h>
-#import <OpenEars/OEEventsObserver.h>
+#import <OpenEars/OEFliteController.h>
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OELogging.h>
+#import <OpenEars/OEAcousticModel.h>
+#import <Slt/Slt.h>
--- DoorwayUsa0/HomeViewController.swift
+++ DoorwayUsa0/HomeViewController.swift
@@ -10,104 +10,14 @@ import UIKit
 
 class HomeViewController: UIViewController, OEEventsObserverDelegate
 {
-    var lmPath: String!
-    var dicPath: String!
-    var words: Array<String> = ["I WALKED THE DOG"]
-    var currentWord: String!
-    
-    var openEarsEventsObserver: OEEventsObserver?
+    var openEarsEngine = OpenEarsEngine()
     
     override func viewDidLoad()
     {
         println("lala")
-        
-        loadOpenEars()
-        startListening()
     }
     
-    func loadOpenEars() {
-        self.openEarsEventsObserver = OEEventsObserver()
-        self.openEarsEventsObserver?.delegate = self
-        
-        var lmGenerator: OELanguageModelGenerator = OELanguageModelGenerator()
-        
-        addWords()
-        var name = "LanguageModelFileStarSaver"
-        lmGenerator.generateLanguageModelFromArray(words, withFilesNamed: name, forAcousticModelAtPath: OEAcousticModel.pathToModel("AcousticModelEnglish"))
-        
-        lmPath = lmGenerator.pathToSuccessfullyGeneratedLanguageModelWithRequestedName(name)
-        dicPath = lmGenerator.pathToSuccessfullyGeneratedDictionaryWithRequestedName(name)
-    }
     
-    func pocketsphinxDidReceiveHypothesis(hypothesis: String!, recognitionScore: String!, utteranceID: String!) {
-        /*
-        if hypothesis == currentWord {
-            //do what you want here when the correct word is recognized
-        }
-*/
-        println("The received hypothesis is \(hypothesis) with a score of \(recognitionScore) and an ID of \(utteranceID)")
-    }
-    
-    func pocketsphinxDidStartListening() {
-        println("Pocketsphinx is now listening.")
-    }
-    
-    func pocketsphinxDidDetectSpeech() {
-        println("Pocketsphinx has detected speech.")
-    }
-    
-    func pocketsphinxDidDetectFinishedSpeech() {
-        println("Pocketsphinx has detected a period of silence, concluding an utterance.")
-    }
-    
-    func pocketsphinxDidStopListening() {
-        println("Pocketsphinx has stopped listening.")
-    }
-    
-    func pocketsphinxDidSuspendRecognition() {
-        println("Pocketsphinx has suspended recognition.")
-    }
-    
-    func pocketsphinxDidResumeRecognition() {
-        println("Pocketsphinx has resumed recognition.")
-    }
-    
-    func pocketsphinxDidChangeLanguageModelToFile(newLanguageModelPathAsString: String, newDictionaryPathAsString: String) {
-        println("Pocketsphinx is now using the following language model: \(newLanguageModelPathAsString) and the following dictionary: \(newDictionaryPathAsString)")
-    }
-    
-    func pocketSphinxContinuousSetupDidFailWithReason(reasonForFailure: String) {
-        println("Listening setup wasn't successful and returned the failure reason: \(reasonForFailure)")
-    }
-    
-    func pocketSphinxContinuousTeardownDidFailWithReason(reasonForFailure: String) {
-        println("Listening teardown wasn't successful and returned the failure reason: \(reasonForFailure)")
-    }
-    
-    func testRecognitionCompleted() {
-        println("A test file that was submitted for recognition is now complete.")
-    }
-    
-    func startListening() {
-        OEPocketsphinxController.sharedInstance().setActive(true, error: nil)
-        OEPocketsphinxController.sharedInstance().startListeningWithLanguageModelAtPath(lmPath, dictionaryAtPath: dicPath, acousticModelAtPath: OEAcousticModel.pathToModel("AcousticModelEnglish"), languageModelIsJSGF: false)
-    }
-    
-    func stopListening() {
-        OEPocketsphinxController.sharedInstance().stopListening()
-    }
-    
-    func addWords() {
-        //add any thing here that you want to be recognized. Must be in capital letters
-        words.append("HELLO")
-        words.append("GOODBYE")
-    }
-    
-    func getNewWord() {
-        var randomWord = Int(arc4random_uniform(UInt32(words.count)))
-        
-        currentWord = words[randomWord]
-    }
 
 }
 

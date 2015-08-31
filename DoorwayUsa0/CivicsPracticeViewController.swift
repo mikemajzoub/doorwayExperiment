d50d0ebfcc9f2fb10cb31194445c39685f628780
mikemajzoub <mikemajzoub@gmail.com>
2015-08-31 13:53:52 -0400

    begin setting up quzzing logic

--- DoorwayUsa0/CivicsPracticeViewController.swift
+++ DoorwayUsa0/CivicsPracticeViewController.swift
@@ -8,10 +8,40 @@
 
 import UIKit
 
-class CivicsPracticeViewController: UIViewController, OEEventsObserverDelegate
+class CivicsPracticeViewController: UIViewController, OpenEarsEngineDelegate
 {
     var dataModel: DataModel!
     var openEarsEngine: OpenEarsEngine!
     
+    override func viewDidAppear(animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        beginPractice()
+    }
     
+    func beginPractice()
+    {
+        // ask question
+        askQuestion()
+        
+        // listen for answer
+        
+        // receive answer
+        
+        // if correct, say, "correct, ANSWER"
+        
+        // if incorrect, say, "incorrect. the correct answer was ANSWER1 or ANSWER2 or ANSWER3"
+        
+        // ask question
+    }
+    
+    func askQuestion()
+    {
+        if let question = dataModel.civicsQuestionBank.nextQuestion()
+        {
+            openEarsEngine.say(question.question)
+            openEarsEngine.startListening()
+        }
+    }
 }

cc03eecebcbc3a0231158b31f94d0e744b3fabda
mikemajzoub <mikemajzoub@gmail.com>
2015-09-03 09:40:58 -0400

    home VC light refactoring

--- DoorwayUsa0/HomeViewController.swift
+++ DoorwayUsa0/HomeViewController.swift
@@ -21,6 +21,7 @@ class HomeViewController: UIViewController
     override func viewDidLoad()
     {
         // cancel out gray for storyboard
+        // TODO: once this is solid, just set to gray in StoryBoard
         civicsView.layer.backgroundColor = UIColor.clearColor().CGColor
         readingView.layer.backgroundColor = UIColor.clearColor().CGColor
         writingView.layer.backgroundColor = UIColor.clearColor().CGColor
@@ -53,16 +54,12 @@ class HomeViewController: UIViewController
         transition.duration = 2
         transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
         
-        // This makes the animation go.
         view.layer.addAnimation(transition, forKey: nil)
     }
     
+    // Creates the circular pie graph that surrounds each button (civics, reading, writing)
     func makeGraphForView(aView: UIView!, andQuestionBank aQuestionBank: CivicsQuestionBank!)
     {
-        
-        
-        println("aQuestionBank: \(aQuestionBank)")
-        
         // make incorrect piece
         let incorrectPiece = makePiePieceForView(aView, forAccuracy: aQuestionBank.percentMastered(), andIsCorrect: false)
         aView.layer.addSublayer(incorrectPiece)
@@ -70,34 +67,35 @@ class HomeViewController: UIViewController
         // make correct piece
         let correctPiece = makePiePieceForView(aView, forAccuracy: aQuestionBank.percentMastered(), andIsCorrect: true)
         aView.layer.addSublayer(correctPiece)
-        
-        // animate pie graph entrance to UI
-        
     }
     
     func makePiePieceForView(view: UIView, forAccuracy accuracy: Float, andIsCorrect isCorrect: Bool) -> CAShapeLayer
     {
+        // Make shape.
         var piePiece = CAShapeLayer()
         piePiece.fillColor = UIColor.clearColor().CGColor
         piePiece.strokeColor = isCorrect ? UIColor.greenColor().CGColor : UIColor.redColor().CGColor
         piePiece.lineWidth = view.bounds.width / 50
         
+        // Get properties of circle.
         let center = CGPointMake(view.bounds.width/2, view.bounds.width/2)
         let radius = view.bounds.width/2 - piePiece.lineWidth/2.0
         
+        // Start at 12 o'clock.
         let startingAngle = (-90).degreesToRadians
         let startingPoint = CGPointMake(center.x + CGFloat(radius) * CGFloat(cosf(startingAngle)), center.y + CGFloat(radius) * CGFloat(sinf(startingAngle)))
-        
-        let radiansToShow = ((360.0 * (isCorrect ? accuracy : 1.0 - accuracy))).degreesToRadians
-        let endingAngle = isCorrect ? startingAngle - radiansToShow : startingAngle + radiansToShow
-        let endingPoint = CGPointMake(center.x + CGFloat(radius) * CGFloat(cosf(endingAngle)), center.y + CGFloat(radius) * CGFloat(sinf(endingAngle)))
-        
         let piePath = UIBezierPath()
         piePath.moveToPoint(startingPoint)
         
+        // If correct, move counterclockwise, else move clockwise.
+        // Distance moved is based upon percent of questions mastered.
+        let radiansToShow = ((360.0 * (isCorrect ? accuracy : 1.0 - accuracy))).degreesToRadians
+        let endingAngle = isCorrect ? startingAngle - radiansToShow : startingAngle + radiansToShow
+        let endingPoint = CGPointMake(center.x + CGFloat(radius) * CGFloat(cosf(endingAngle)), center.y + CGFloat(radius) * CGFloat(sinf(endingAngle)))
         let isClockwise = !isCorrect
         piePath.addArcWithCenter(center, radius: CGFloat(radius), startAngle: CGFloat(startingAngle), endAngle: CGFloat(endingAngle), clockwise: isClockwise)
         
+        // Add path to shape.
         piePiece.path = piePath.CGPath
         
         return piePiece

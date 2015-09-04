7bce3f584a30c4667936617b510f9b31f62addc3
mikemajzoub <mikemajzoub@gmail.com>
2015-09-04 19:44:09 -0400

    gitignore

---
+++ DoorwayUsa0/ApiKeys.swift
@@ -0,0 +1,12 @@
+//
+//  ApiKeys.swift
+//  DoorwayUsa0
+//
+//  Created by Michael Majzoub on 9/4/15.
+//  Copyright (c) 2015 mikemajzoub. All rights reserved.
+//
+
+import Foundation
+
+// GITIGNORE THIS!!!!!!!!!
+

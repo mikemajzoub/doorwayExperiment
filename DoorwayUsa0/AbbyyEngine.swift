d4997a6b60722a1224dc2f14f898b9e26587d28d
mikemajzoub <mikemajzoub@gmail.com>
2015-09-06 13:23:06 -0400

    preliminary init AbbyyEngine

--- DoorwayUsa0/AbbyyEngine.swift
+++ DoorwayUsa0/AbbyyEngine.swift
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 mikemajzoub. All rights reserved.
 //
 
+import Foundation
+
 protocol AbbyyEngineDelegate: class
 {
     
@@ -13,19 +15,76 @@ protocol AbbyyEngineDelegate: class
 
 class AbbyyEngine: NSObject
 {
+    let kInstallationId = "InstallationId"
+    let kActivationUrlMinusDeviceId = "http://cloud.ocrsdk.com/activateNewInstallation?deviceId="
+    let kHttpHeaderField = "Authorization"
+    
     weak var delegate: AbbyyEngineDelegate?
     
     var applicationId: String!
     var applicationPassword: String!
+    var fullId: String!
     
+    // MARK: - Init
     override init()
     {
         super.init()
         
         applicationId = ABBYY_APPLICATION_ID
         applicationPassword = ABBYY_APPLICATION_PASSWORD
+        
+        setUpInstallationId()
+        let installationId = NSUserDefaults.standardUserDefaults().stringForKey(kInstallationId)
+        fullId = "\(applicationId)\(installationId)"
     }
     
+    func setUpInstallationId()
+    {
+        if NSUserDefaults.standardUserDefaults().stringForKey(kInstallationId) == nil
+        {
+            let deviceId = UIDevice.currentDevice().identifierForVendor.UUIDString
+            
+            let installationId = activateNewInstallationForDeviceId(deviceId)
+            
+            NSUserDefaults.standardUserDefaults().setValue(installationId, forKey: kInstallationId)
+        }
+    }
+    
+    func activateNewInstallationForDeviceId(deviceId: String) -> String
+    {
+        
+        let installationId: String
+        
+        // set up request
+        let activationUrl = NSURL(string: kActivationUrlMinusDeviceId + deviceId)
+        let request = NSMutableURLRequest(URL: activationUrl!)
+        let authentication = "Basic \(applicationId):\(applicationPassword)"
+        request.setValue(authentication, forKey: kHttpHeaderField)
+        
+        var error: NSError
+        
+        // TODO:
+        let responseData = NSURLConnection.sendSynchronousRequest(request, returningResponse: 0, error: &error)
+        
+        // TODO:
+        if error == nil
+        {
+            parser = NSXMLParser(data: responseData)
+            parser.delegate = self
+            
+            // In sample code, I don't see where parser is setting installation id. look into this.
+            installationId = "TODO:"
+        }
+        else
+        {
+            alert = UIAlertView(title: "Error", message: error.localizedDescription, delegate: nil, cancelButtonTitle: "OK", otherButtonTitles: nil, nil)
+            alert.show()
+        }
+        
+        return installationId
+    }
+    
+    // MARK: - Logic
     func processImage(takenPicture: UIImage?)
     {
         if let image = takenPicture
